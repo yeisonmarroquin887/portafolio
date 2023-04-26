337b1af0422766429017ceb1758d994cefdb363a
yeisonmarroquin <andresmarroquin887@gmail.com>
2023-04-26 18:57:44 -0500

    Portafolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,37 @@
         }
     });
 
-    
+    {
     const runHTML = document.querySelector(".briefcase_menu")
     const bxcartHTML = document.querySelector(".menu")
     
     runHTML.addEventListener("click", function (){
         bxcartHTML.classList.toggle("run__show")
     })
+
+     
+    const volver = document.querySelector(".home")
+    const ingre = document.querySelector(".menu")
+    volver.addEventListener("click", function () {
+        ingre.classList.toggle("run__show")
+    })
+    }
+
+{
     
+    const proyeHTML = document.querySelector(".new")
+    const pro = document.querySelector(".proye")
     
+    proyeHTML.addEventListener("click", function (){
+        pro.classList.toggle("proye_show")
+    })
     
+
+
+
+    const volver = document.querySelector(".volver")
+    const ingre = document.querySelector(".proye")
+    volver.addEventListener("click", function () {
+        ingre.classList.toggle("proye_show")
+    })
+}
